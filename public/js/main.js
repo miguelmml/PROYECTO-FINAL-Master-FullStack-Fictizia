f02d7bb1de67b8442defdbbc4b95e0424ba470f3
Miguel Martín-Maestro López <miguelmml1993@gmail.com>
2020-09-15 11:23:22 +0200

    refactor of views

--- public/js/main.js
+++ public/js/main.js
@@ -34,17 +34,6 @@ function checkUser () {
       localStorage.removeItem('gamesAppUserEmail')
       window.location.replace('/')
     })
-
-    const btnSearch = document.getElementById('search')
-
-    btnSearch.addEventListener('click', (e) => {
-      e.preventDefault()
-      const searchItemId = document.getElementById('searchItem').value
-
-      if (searchItemId !== '') {
-        window.location = `../search/${searchItemId}`
-      }
-    })
   } catch (error) {
     console.error(error)
   }
